8d3e51aa32ad0a2d4b6408a0723897eb91a86a84
Davis <giangdavis@gmail.com>
2019-11-09 18:31:40 -0800

    Created Server, User, Room, And Lobby
    Can set up a server that listens and creates a connection list of users. Not receiving and outputting messages yet.

---
+++ chatClasses.py
@@ -0,0 +1,40 @@
+# structure starting w/ 3 tiers. lobby > rooms > users 
+
+import socket 
+
+class User: 
+	def __init__(self, socket, name): 
+		socket.setblock(0)  # turn off blocking
+		self.socket = socket 
+		self.name = name	
+
+
+class Room:
+	def __init__(self, name, password=""):
+		self.users = []   # a list of sockets
+		self.name = name
+		self.password = password  # rooms can be locked with a password 
+
+	def removeUser(self, user): 
+		self.users.remove(user)
+		msg = user.name.encode(encoding = 'UTF-8') + " left the room\n"
+		self.transmit(user, msg) 
+
+	def transmit(self, sender, msg):
+		msg = send.name.encode() + ":" + msg
+		for users in self.users
+			user.socket.sendall(msg) 
+
+	# can add a welcome msg 
+
+# class feed ? haven't decided where this should go.. 
+# thinking its server side.. and server will handle outputting
+# all room msgs to all its clients 
+
+class Lobby: 
+	def __init__(self): 
+		self.rooms = {}  #dict, {room name : room} 
+		self.roomUsers = {}  #dict contain users & rooms they're in. {user : room name} 
+
+	def welcomeMsg(self, user):
+		user.socket.sendall("Welcome to ____\n Enter a username: ")
---
+++ chatServer.py
@@ -0,0 +1,40 @@
+import socket, sys, select
+
+# from classes.py import User, Room, Lobby
+# import classes.py
+
+PORT = 54543 
+MAXCLIENTS = 25 
+READBUFFERSIZE = 4096
+
+# get the host IP address and start listening for connections
+
+arguments = len(sys.argv) - 1 
+if arguments == 1: 
+	hostAddr = sys.argv[1]
+else: 
+	hostAddr = ''
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # set the socket option, to continually listen. see doc 
+s.setblocking(0)  # turns off socket blocking
+s.bind((hostAddr, PORT))  # assigns host ip add and port to socket  
+s.listen(MAXCLIENTS)  
+
+print("Server set up & listening! Connect with address: " , hostAddr)
+
+connectionList = [s]  # create a list of users, starting w/ host 
+
+while True:  # always listening for connections & messages 
+	readUsers, writeUsers, errorUsers = select.select(connectionList, [], []) 
+	for user in readUsers:  # loop through users 
+		if user is s:  # if user is a socket, new connection
+			newSocket, addr = user.accept()
+			newUser = User(newSocket, "")
+			connectionList.append(newUser)
+
+		else:  # new messages 
+			newMsg = user.socket.recv(READBUFFERSIZE) 
+			if newMsg == 0 :  # message contents are empty 
+				user.socket.close() 
+				connection_list.remove(user)
